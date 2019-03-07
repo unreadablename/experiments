a9d89d198cd0d1a5a911758d47589b7e9989cb27
Oleksandr Pidlisnyi <oleksandr.pidlisnyi@gmail.com>
2019-03-07 13:47:41 +0200

    test fixed

--- src/app.controller.spec.ts
+++ src/app.controller.spec.ts
@@ -16,7 +16,7 @@ describe('AppController', () => {
 
   describe('root', () => {
     it('should return "Hello World!"', () => {
-      expect(appController.getHello()).toBe('Hello World!1');
+      expect(appController.getHello()).toBe('Hello World!');
     });
   });
 });
